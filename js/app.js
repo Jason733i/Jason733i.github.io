706596963e479dfcc960a6a55e18de16e4afefe9
Jason Berry <uniqueberr@gmail.com>
2013-12-30 21:09:59 -0500

    Using Angular Seed to create app.

---
+++ js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Declare app level module which depends on filters, and services
+angular.module('myApp', [
+        'ngRoute',
+        'myApp.filters',
+        'myApp.services',
+        'myApp.directives',
+        'myApp.controllers'
+    ]).
+    config(['$routeProvider', function($routeProvider) {
+        $routeProvider.when('/blog', {templateUrl: 'partials/blog.html', controller: 'BlogController'});
+        $routeProvider.when('/bio', {templateUrl: 'partials/bio.html', controller: 'BioController'});
+        $routeProvider.otherwise({redirectTo: '/'});
+    }]);
---
+++ js/controllers.js
@@ -0,0 +1,7 @@
+'use strict';
+
+/* Controllers */
+angular.module('myApp.controllers', []).
+    controller('MainController', function($scope) {
+        $scope.welcome = "Welcome to My Website!";
+    });
