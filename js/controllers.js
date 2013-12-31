db4d9531c182d44fedeec14be465e45a93ff95c4
Jason Berry <uniqueberr@gmail.com>
2013-12-30 21:11:06 -0500

    Adding missing controllers

--- js/controllers.js
+++ js/controllers.js
@@ -4,4 +4,10 @@
 angular.module('myApp.controllers', []).
     controller('MainController', function($scope) {
         $scope.welcome = "Welcome to My Website!";
+    }).
+    controller('BlogController', function($scope) {
+
+    }).
+    controller('BioController', function($scope) {
+
     });
