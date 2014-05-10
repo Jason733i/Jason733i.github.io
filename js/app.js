a78e0128e1daf7184115741c056a8e384fa18bbe
Jason Berry <uniqueberr@gmail.com>
2014-05-10 11:37:05 -0400

    Removing bio page

--- js/app.js
+++ js/app.js
@@ -11,7 +11,6 @@ angular.module('myApp', [
     config(function($routeProvider) {
         $routeProvider.when('/', {templateUrl: 'partials/home.html', controller: 'HomeController'});
         $routeProvider.when('/blog', {templateUrl: 'partials/blog.html', controller: 'BlogController'});
-        $routeProvider.when('/bio', {templateUrl: 'partials/bio.html', controller: 'BioController'});
         $routeProvider.when('/projects', {templateUrl: 'partials/projects.html', controller: 'ProjectsController'});
         $routeProvider.otherwise({redirectTo: '/'});
     }).
